21fcfc9fe73650c749e9d1967e5ac84762da7714
Michael Plaskett <plaskettmichael@gmail.com>
2023-07-26 07:40:21 +0000

    First Bash Script

---
+++ Hellowworld.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Script Name:                  My First Bash Script
+# Author:                       Michael Plaskett    
+# Date of latest revision:      07/25/2023
+# Purpose:                      Purpose
+
+# Declaration of variables
+
+# Declaration of functions
+
+
+# Main
+#!/bin/bash
+echo "Hello World"
+
+
+# End
