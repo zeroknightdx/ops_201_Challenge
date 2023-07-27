b8795348f701c914deaafd3ac2f5f250b3c17acc
Michael Plaskett <plaskettmichael@gmail.com>
2023-07-27 19:52:05 +0000

    Fun

--- create_directories.sh
+++ create_directories.sh
@@ -2,22 +2,23 @@
 
 # Script Name:                  My First Bash Script
 # Author:                       Michael Plaskett    
-# Date of latest revision:      07/26/2023
+# Date of latest revision:      07/27/2023
 # Purpose:                     
 # Resource Used: 
-# Create the first directory
-mkdir dir1
-
-# Create the second directory
-mkdir dir2
-
-# Create the third directory
-mkdir dir3
-
-# Create the fourth directory
-mkdir dir4
-
 # Main
 
+# Names of the four directories in an array
+directories=("dir1" "dir2" "dir3" "dir4")
 
+# Loop through the array to create a new .txt file in each directory
+for dir in "${directories[@]}"; do
+    # Create the directory if it doesn't exist
+    mkdir -p "$dir"
+    
+    # Create a new .txt file in the directory
+    touch "$dir/file.txt"
+done
+
+# Display a message after creating the files
+echo "New .txt files created in the directories."
 # End
