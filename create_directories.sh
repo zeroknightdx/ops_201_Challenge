6241e5b87b56587000c6b26ea9fe0958aa230cf6
Michael Plaskett <plaskettmichael@gmail.com>
2023-07-27 18:46:04 +0000

    Fun_

---
+++ create_directories.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Script Name:                  My First Bash Script
+# Author:                       Michael Plaskett    
+# Date of latest revision:      07/26/2023
+# Purpose:                     
+# Resource Used: 
+# Create the first directory
+mkdir dir1
+
+# Create the second directory
+mkdir dir2
+
+# Create the third directory
+mkdir dir3
+
+# Create the fourth directory
+mkdir dir4
+
+# Main
+
+
+# End
